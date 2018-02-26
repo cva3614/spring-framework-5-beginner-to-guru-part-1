d82fdeea146e353f271775551491ba1fd759ce6e
js <jsbsmp@gmail.com>
2018-02-26 12:56:20 +0200

    Spring Configuration: Multiple Property Files

--- src/main/java/com/udemy/dependencyinjectiondemo/DependencyInjectionDemoApplication.java
+++ src/main/java/com/udemy/dependencyinjectiondemo/DependencyInjectionDemoApplication.java
@@ -2,6 +2,7 @@ package com.udemy.dependencyinjectiondemo;
 
 import com.udemy.dependencyinjectiondemo.controllers.*;
 import com.udemy.dependencyinjectiondemo.fakebeans.FakeDataSource;
+import com.udemy.dependencyinjectiondemo.fakebeans.FakeJmsBroker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -9,13 +10,15 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class DependencyInjectionDemoApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		ApplicationContext context = SpringApplication.run(DependencyInjectionDemoApplication.class, args);
-		SimpleController controller = (SimpleController) context.getBean("simpleController");
-		controller.hello();
+        ApplicationContext context = SpringApplication.run(DependencyInjectionDemoApplication.class, args);
+        SimpleController controller = (SimpleController) context.getBean("simpleController");
 
-		FakeDataSource fakeDataSource = context.getBean(FakeDataSource.class);
-		System.out.println(fakeDataSource.getUsername());
-	}
+        FakeDataSource fakeDataSource = context.getBean(FakeDataSource.class);
+        System.out.println("fakeDataSource username: " + fakeDataSource.getUsername());
+
+        FakeJmsBroker fakeJmsBroker = context.getBean(FakeJmsBroker.class);
+        System.out.println("fakeJmsBroker username: " + fakeJmsBroker.getUsername());
+    }
 }
--- src/main/java/com/udemy/dependencyinjectiondemo/config/PropertyConfig.java
+++ src/main/java/com/udemy/dependencyinjectiondemo/config/PropertyConfig.java
@@ -1,16 +1,18 @@
 package com.udemy.dependencyinjectiondemo.config;
 
 import com.udemy.dependencyinjectiondemo.fakebeans.FakeDataSource;
+import com.udemy.dependencyinjectiondemo.fakebeans.FakeJmsBroker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
 @Configuration
-@PropertySource("classpath:datasource.properties")
+@PropertySources({@PropertySource("classpath:datasource.properties"), @PropertySource("classpath:jms.properties")})
 public class PropertyConfig {
 
     @Autowired
@@ -25,8 +27,17 @@ public class PropertyConfig {
     @Value("${dburl}")
     String dbUrl;
 
+    @Value("${jms.username}")
+    String jmsUsername;
+
+    @Value("${jms.password}")
+    String jmsPassoword;
+
+    @Value("${jms.url}")
+    String jmsUrl;
+
     @Bean
-    public FakeDataSource fakeDataSource(){
+    public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
         fakeDataSource.setUsername(env.getProperty("USERNAME"));
         fakeDataSource.setPassword(password);
@@ -35,7 +46,16 @@ public class PropertyConfig {
     }
 
     @Bean
-    public static PropertySourcesPlaceholderConfigurer properties(){
+    public FakeJmsBroker fakeJmsBroker() {
+        FakeJmsBroker jmsBroker = new FakeJmsBroker();
+        jmsBroker.setUsername(jmsUsername);
+        jmsBroker.setPassword(jmsPassoword);
+        jmsBroker.setUrl(jmsUrl);
+        return jmsBroker;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties() {
         return new PropertySourcesPlaceholderConfigurer();
     }
 }
